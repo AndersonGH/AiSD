a89e305813537eb265ae0dbe0ac3787aab5a24c3
root <root@DESKTOP-312SDD5.localdomain>
2018-10-04 22:41:58 +0300

    fixed test.

--- 7381/TrushnikovAP/lab2/Source/main.cpp
+++ 7381/TrushnikovAP/lab2/Source/main.cpp
@@ -15,13 +15,16 @@ int main(int argc, char* argv[]){
     char *in[len_in];
 
 
-    if(!input(argc,argv,Stack,in,len_in)) // функция считывания данных
-        std::cout <<"Oshibka skobok."<< std::endl << std::endl;  // вернет false при неправильной расстановке скобок
+    if(!input(argc,argv,Stack,in,len_in)){ // функция считывания данных
+        std::cout <<"Oshibka skobok."<< std::endl << std::endl;
+		
+	}		// вернет false при неправильной расстановке скобок
     else{
         lisp.create_lisp(len_in,in,lisp); // метод класса lisp для создания иерархического списка
         bool check = true;
         if(lisp.root->isAtom_num(lisp.root)){
                 std::cout << "Incorrect expression" << std::endl << std::endl;
+				 return 0;
         }
         else{
             syntax_check(lisp.root,check);
