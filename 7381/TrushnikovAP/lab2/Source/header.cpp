c9b6016f0805d13844ca76e603bf85f71e1d554d
root <root@DESKTOP-312SDD5.localdomain>
2018-10-07 15:28:54 +0300

    code fixed

--- 7381/TrushnikovAP/lab2/Source/header.cpp
+++ 7381/TrushnikovAP/lab2/Source/header.cpp
@@ -2,11 +2,18 @@
 #include <vector>
 #include <cstring>
 
-void numTostr(int num, char *str){
+void numTostr(int num, char *str){ // функция преобразования int в char*
     int i =0;
+    bool neg = false;
     int divider=1;
     int count = 1;
-    if(num>9){
+    if(num < 0){ // если число отрицаиельное делаем его положительным
+        num=abs(num);
+        neg = true;
+        i++;
+    }
+
+    if(num>9){  // если число больше девяти определяем количество десятков в нем
         while(count !=0){
             divider*=10;
             count = num/divider;
@@ -14,7 +21,7 @@ void numTostr(int num, char *str){
         }
     divider/=10;
     }
-    while(divider!=0){
+    while(divider!=0){ // предобразовываем число в строку
         int res;
         res = num/divider;
         if(res>=10)
@@ -26,64 +33,93 @@ void numTostr(int num, char *str){
         divider/=10;
         i++;
     }
+    if(neg)
+        str[0] = '-';
     str[i] = '\0';
 
 }
 
-void syntax_check(class Lisp_Node *node,bool &check){
-    if(node == NULL)
+void syntax_check(class Lisp_Node *node,bool &check){//функция для проверки на корректность ариф выражения
+    if(node == NULL){
+        std::cout<< "Null" << std::endl;
         return;
+    }
 
-    if(node->is_pair(node) && !node->is_Nill(node)){
+    if(node->is_pair(node)){
         std::cout<< "is pair" << std::endl;
     }
     else if(node->is_Nill(node)){
         std::cout<< "Nill "<< std::endl;
-
     }
     else if(node->isAtom_num(node)){
-        std::cout<< "atom: " << node->s.num << std::endl;
-
+        std::cout<< "atom num: " << node->s.num << std::endl;
     }
     else if(node->isAtom_sign(node)){
-        std::cout<< "atom: " << node->s.sign << std::endl;
-        if(node->s.sign == '/')
-            if(node->right!=NULL && node->right->isAtom_num(node->right))
-                if(node->right->s.num == 0)
-                    check = false;
-
+        std::cout<< "atom sign: " << node->s.sign << std::endl;
     }
 
-    if(node->isAtom_sign(node) && !node->is_pair(node)){
-        Lisp_Node *error = node;
-        bool check_num = true;
-        while(error->right!=NULL){
-            if(error->right->isAtom_num(error->right))
-                check_num =false;
-            if(error->right->isAtom_sign(error->right)){
-                check = false;
-                break;
+    if(node->isAtom_sign(node) && check){ // если встретили знак деления проверяем нету ли нуля правее него
+        if(node->s.sign == '/'){
+            Lisp_Node *error = node;
+            while(error != NULL){
+                if(error->isAtom_num(error) && error->s.num == 0){
+                    check = false;
+                    break;
+                }
+                error = error->right;
             }
 
-        error = error->right;
         }
 
-        if(check_num)
-            check = false;
+
     }
 
-    if(node->right == NULL)
-        if(node->is_pair(node))
+    if(node->isAtom_num(node) && check){   //если встретили число проверяем нету ли знака правее него
+         Lisp_Node *error = node;
+         while(error != NULL){
+             if(error->isAtom_sign(error)){
+                 check = false;
+                 break;
+             }
+             error = error->right;
+         }
+
+
+    }
+
+    if(node->is_pair(node) ){ // если встретили pair проверяем является ли его нижний элемент знаком
+        if(node->right == NULL)
             check = false;
 
-    syntax_check(node->s.bottom,check);
+        if(node->is_Nill(node->bottom)){
+            Lisp_Node *error = node->bottom;
+            while(error->is_Nill(error)){
+                error = error->right;
+                if(error == NULL){
+                    check = false;
+                    break;
+                }
+            }
+            if(check){
+                if(error->isAtom_num(error) || error->is_pair(error))
+                    check = false;
+
+            }
+
+        }
+    }
+
+    std::cout << "go bottom -> ";
+    syntax_check(node->bottom,check);
+    std::cout << "go right  -> ";
     syntax_check(node->right,check);
 
 
     return;
 }
 
-void paste_node(class Lisp_Node *pred,class Lisp_Node *paste){
+
+void paste_node(class Lisp_Node *pred,class Lisp_Node *paste){ // функция вставляет атом или pair между двумя другими
 
     class Lisp_Node *copy = new Lisp_Node;
     copy=pred->right;
@@ -92,10 +128,12 @@ void paste_node(class Lisp_Node *pred,class Lisp_Node *paste){
 }
 
 
-void resize(int len,char *in[]){
+void resize(int &len,char ** &in){// функция увеличения размера двумерного массива
     int l_copy = len;
-    char *copy[l_copy];
-    for(int i =0;i<len;i++){
+
+    char **copy = new char* [l_copy];
+
+    for(int i =1;i<len;i++){
         if(strlen(in[i]) == 1)
             copy[i] = new char [2];
         else
@@ -103,27 +141,34 @@ void resize(int len,char *in[]){
         strcpy(copy[i],in[i]);
     }
 
-    for(int i =0;i<len;i++){
-         delete in[i];
+    for(int i = 0;i<len;i++){
+         delete [] in[i];
     }
+    delete  [] in;
 
     len=len*2;
     in = new char* [len];
-    for(int i =0;i<l_copy;i++){
-        if(strlen(copy[i]) == 1)
+    for(int i =1;i<l_copy;i++){
+        if(strlen(copy[i]) == 1){
             in[i] = new char [2];
-        else
+            strcpy(in[i],copy[i]);
+        }
+        else{
             in[i] = new char [100];
+            strcpy(in[i],copy[i]);
+        }
 
-        strcpy(in[i],copy[i]);
+
+
     }
 
     for(int i =0;i<l_copy;i++){
-        delete copy[i];
+        delete [] copy[i];
     }
+    delete  [] copy;
 }
 
-bool input(int argc, char* argv[],char **in,int &len){
+bool input(int argc, char* argv[],char ** &in,int &len){
 
    std:: stack <char> brackets;
    bool check_for_brackets=true;
--- 7381/TrushnikovAP/lab2/Source/main.cpp
+++ 7381/TrushnikovAP/lab2/Source/main.cpp
@@ -4,34 +4,40 @@
 
 
 int main(int argc, char* argv[]){
-
     Lisp lisp;
-    int len_in=30;
+    int len_in=20;
 
-    if(argc>1)
+    if(argc > 1){
         len_in=argc;
+    }
+    else
+        argc = 0;
 
-    char *in[len_in];
+    char **in = new char* [len_in];
 
 
     if(!input(argc,argv,in,len_in)){ // функция считывания данных
-        std::cout <<"Oshibka skobok."<< std::endl << std::endl;
-		
-	}		// вернет false при неправильной расстановке скобок
+        argc = 1;				     // вернет false при неправильной расстановке скобок
+        std::cout <<"Oshibka skobok."<< std::endl << std::endl;		
+	}		
     else{
-        lisp.create_lisp(len_in,in,lisp); // метод класса lisp для создания иерархического списка
-        bool check = true;
+        bool check = true;// индикатор корректности ариф выражения true если оно корректно
+        lisp.create_lisp(len_in,in,lisp); //вызов  метода класса lisp для создания иерархического списка
+        std::cout<<std::endl;
+		if(len_in > 3){     //выполним проверку первого атома на наличие атома знака
+			Lisp_Node *node = lisp.root;// если количество элементов ариф варажения больше 2
 
-        Lisp_Node *node = lisp.root;
+			if(node->is_Nill(node)) // если у нас певый элемент списка - Nill идем в право от него пока не встретим первый атом
+				while(node->is_Nill(node))
+					node = node->right;
 
-        if(node->is_Nill(node))
-            while(node->is_Nill(node))
-                node = node->right;
+			if(node->isAtom_num(node))//если первый атом число - выражение арифметическое некорректно
+				check = false;
+		}
+		if(len_in == 4 )// если кол во элементов равно 4 значит это либо (знак) либо (число), что некорректно
+			check = false;
 
-        if(node->isAtom_num(node))
-            check = false;
-
-        syntax_check(lisp.root,check);
+        syntax_check(lisp.root,check);//вызов функции проверки на корректность иерархического списка
 
         if(check)
             std::cout << "Correct" << std::endl << std::endl;
@@ -39,6 +45,17 @@ int main(int argc, char* argv[]){
             std::cout << "Incorrect expression" << std::endl << std::endl;
 
     }
+
+    if(argc == 0)
+        for( int i = 0 ; i < len_in; i++){ // очищаем двумерный массив входных данных
+            delete  [] in [i]; 			   // если память под него выделялась динамически
+        }
+
+    delete  [] in;
+
+    Lisp_Node *del = lisp.root;
+    if(del)				//вызов деструктора иерархического списка
+        delete del;
 
     return 0;
 }
